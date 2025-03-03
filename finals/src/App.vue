5d51d9a7fa1cd2f11979ad68332202ce9fd6caa2
Marianne Ellise P. Lama <lems.mariannelama@gmail.com>
2025-03-03 14:33:16 +0000

    dad

--- finals/src/App.vue
+++ finals/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue';
-import CommentSection from './components/CommentSection.vue'
+import Comment from './components/Comment.vue'
+import CommentForm from './components/CommentForm.vue'
 
 const showForm = ref(false);
 
