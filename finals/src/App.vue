b89f1d21ca814c05b57b1bc33252cde3c28abffa
Marianne Ellise P. Lama <lems.mariannelama@gmail.com>
2025-03-04 07:12:41 +0000

    mm

--- finals/src/App.vue
+++ finals/src/App.vue
@@ -1,27 +1,8 @@
-import { ref, onMounted } from 'vue'
-import { supabase } from ',/lib/supabaseClient'
-
-count comments = ref([])
-
-async function getComments() {
-    const { data } = await supabase.from('comments').select()
-    comments.value = data
-}
-
-onMounted(() => {
-    getComments()
-})
-
 <template>
-    <ul>
-      <li v-for="comments in comment" :key="comment.id">{{ comment.name }}</li>
-    </ul>
+  <comment-form/>
+  <comment/>
+   
 
-    <comment-form/>
-    <comment/>
-     
-  
-  </template>
-  
+</template>
 
-  <script></script>
+<script></script>
--- finals/src/main.js
+++ finals/src/main.js
@@ -5,6 +5,7 @@ import Comment from './components/Comment.vue'
 import { createClient } from '@supabase/supabase-js';
 
 const app = createApp(App)
+
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
 app.mount('#app')
