76e88cb2de539409ebdf8ea8a5a821808e30759d
Marianne Ellise P. Lama <lems.mariannelama@gmail.com>
2025-03-04 03:03:26 +0000

    srwa

--- finals/src/main.js
+++ finals/src/main.js
@@ -3,18 +3,19 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createClient } from '@supabase/supabase-js'
 
-if (!supabaseUrl || !supabaseAnonKey) {
-    console.error('Supabase URL or ANON KEY is missing. Check your environment variables.');
-}
-
 const supabaseUrl = 'https://jezhjxodrkujwzixokmz.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImplemhqeG9kcmt1and6aXhva216Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwMDczODksImV4cCI6MjA1NjU4MzM4OX0.5EgaMoO3XsfWWj3v-018sy0ZY4qaEVI-oDhIFMfG0Pc';
 
 if (!supabaseUrl || !supabaseAnonKey) {
-    console.error("Supabase credentials are missing! Check your .env file.");
+    console.error("❌ Supabase credentials are missing! Check your .env file.");
+  } else {
+    console.log("✅ Supabase URL:", supabaseUrl);
+    console.log("✅ Supabase Key:", supabaseAnonKey);
   }
 
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+export { supabase };
 
 const app = createApp(App);
 
--- finals/vite.config.js
+++ finals/vite.config.js
@@ -1,18 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import dotenv from 'dotenv';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+dotenv.config();
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+});
